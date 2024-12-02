896cf0ff2bd61b58fc092660c2979c857c8db9f7
AbrahamGCh <1823110013@untels.edu.pe>
2024-12-02 17:02:29 -0500

    Semana14

---
+++ MDAM_Sem11_ProyectoyApplication/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MDAM_Sem11_Proyectoy Application"
+include(":app")
+ 
